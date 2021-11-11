8e1a0eb71c3cee39883d925f0d400643aebdab37
Rysias <jvhithamar@gmail.com>
2021-11-11 12:40:56 +0100

    create hopefully ready version of topic creator!

--- create_topic.py
+++ create_topic.py
@@ -1,34 +1,56 @@
 from pathlib import Path
+from typing import Union
 
 import numpy as np
 import pandas as pd
-import requests
+import argparse
 from bertopic import BERTopic
-from sklearn.feature_extraction.text import CountVectorizer
-
-DATA_DIR = Path("./output")
 
 
-def load_text_url(text_url: str):
-    """Inputs a URL of a newline seperated text-file and returns a list"""
-    response = requests.get(text_url)
-    return response.text.split("\n")
+def load_docs(data_path: Union[str, Path], text_col="cleantext") -> np.ndarray:
+    df = pd.read_csv(Path(data_path))
+    return df[text_col].values
 
 
-embs = np.load(DATA_DIR / "embs.npy")
-emb_id = embs[:, 0]
-embeddings = embs[:, 1:]
+def load_embeds(embed_path: Union[str, Path]):
+    raw_embeds = np.load(embed_path)
+    return raw_embeds[:, 1:]
 
 
-docs = pd.read_csv(DATA_DIR / "full_tweets.csv")["text"]
+def main(args):
+    print("loadin data...")
+    docs = load_docs(args.data_path)
+    embeddings = load_embeds(args.embed_path)
+    print("bootin model...")
+    topic_model = BERTopic(nr_topics=10)
+    print("fittin model...")
+    topics, probs = topic_model.fit_transform(docs, embeddings)
+    print("savin data...")
+    preds_df = pd.DataFrame(
+        list(zip(topics, probs, docs)), columns=["topic", "prob", "doc"]
+    )
+    preds_df.to_csv(Path(args.save_path) / "doc_topics.csv", index=False)
+    print("savin model...")
+    topic_model.save(
+        str(Path(args.save_path) / "topic_model"), save_embedding_model=False
+    )
+    print("all done!")
 
-STOP_WORD_URL = "https://gist.githubusercontent.com/berteltorp/0cf8a0c7afea7f25ed754f24cfc2467b/raw/305d8e3930cc419e909d49d4b489c9773f75b2d6/stopord.txt"
-STOP_WORDS = load_text_url(STOP_WORD_URL)
-vectorizer_model = CountVectorizer(stop_words=STOP_WORDS)
 
-topic_model = BERTopic(vectorizer_model=vectorizer_model, nr_topics=3)
-
-topics, probs = topic_model.fit_transform(docs, embeddings)
-
-topic_model.get_topics()
-docs[2085]
+if __name__ == "__main__":
+    my_parser = argparse.ArgumentParser(description="Embed Documents")
+    my_parser.add_argument(
+        "--data_path", type=str, help="Gives the path to the data file (a csv)"
+    )
+    my_parser.add_argument(
+        "--embedding_path",
+        type=str,
+        help="Gives the path to the embeddings (.npy)",
+    )
+    my_parser.add_argument(
+        "--save_path",
+        type=str,
+        help="Path to directory to save stuff",
+    )
+    args = my_parser.parse_args()
+    main(args)
