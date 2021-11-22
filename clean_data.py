872398537b272657ecdf82bb258107587c6dea18
Rysias <jvhithamar@gmail.com>
2021-11-22 13:44:04 +0100

    clean up cleaning pipeline

--- clean_data.py
+++ clean_data.py
@@ -4,15 +4,9 @@ Clean the twitter data for embedding
 from os import replace
 from pathlib import Path
 import pandas as pd
-import re
 import numpy as np
-import functools
 
 DATA_DIR = Path("./output")
-
-
-def compose(*functions):
-    return functools.reduce(lambda f, g: lambda x: f(g(x)), functions)
 
 
 def find_file(path, pattern="*"):
@@ -27,29 +21,6 @@ def preprocess(text):
         t = "http" if t.startswith("http") else t
         new_text.append(t)
     return " ".join(new_text)
-
-
-def remove_pattern(text_col: pd.Series, pattern: str) -> pd.Series:
-    return text_col.str.replace(pattern, "")
-
-
-def remove_usernames(text_col: pd.Series) -> pd.Series:
-    pattern = "@\w+"
-    return remove_pattern(text_col, pattern)
-
-
-def remove_urls(text_col: pd.Series) -> pd.Series:
-    pattern = "https?://\w+\\.\w+/\w+"
-    return remove_pattern(text_col, pattern)
-
-
-def clean_col(col, *functions):
-    clean_pipeline = compose(*functions)
-    return clean_pipeline(col)
-
-
-def clean_textcol(text_col):
-    return clean_col(text_col, remove_urls, remove_usernames)
 
 
 df = pd.read_csv(
